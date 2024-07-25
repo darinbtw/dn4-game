9e9db6d44a0f6f1458ecc184f3c88dcd1eb69608
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-07-25 21:44:04 +0300

    the player is moving right and left side

--- godot game/playerok.gd
+++ godot game/playerok.gd
@@ -1,28 +1,16 @@
-extends CharacterBody2D
+extends Node2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var speed = 200  # Скорость движения
+var jump_force = 500  # Сила прыжка
+var vel = Vector2()  # Вектор скорости
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	vel = Vector2()  # Сбрасываем скорость
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("player_left"):
+		vel.x -= speed  # Влево
+	if Input.is_action_pressed("player_right"):
+		vel.x += speed  # Вправо
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	# Обновляем позицию игрока
+	position += vel * delta  # Изменяем позицию в зависимости от скорости

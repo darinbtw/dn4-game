72c7fe6ffccea0e8e3f5aa6ca7a6cee3609738ba
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-10-04 12:12:39 +0300

    added skeletone

---
+++ godot game/skeleton.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var chase = false
+var speed = 100
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	var player = $"../../Player/Player"
+	var direction = (player.position - self.position).normalized()
+	if chase == true:
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = 0
+	move_and_slide()
+
+
+func _on_detector_body_entered(body):
+	if body.name == 'Player':
+		chase = true
+
+
+func _on_detector_body_exited(body):
+	if body.name == 'Player':
+		chase = false

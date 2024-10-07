a5494545f244abeded936db4853b12c531505cb5
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-10-07 17:04:52 +0300

    update

--- godot game/Player/player.gd
+++ godot game/Player/player.gd
@@ -9,6 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = $AnimatedSprite2D
 var health = 100
+var gold = 0
 
 func _physics_process(delta):
 	# Add the gravity.

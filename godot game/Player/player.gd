f8e99cea706d6c2405fdbe53757e82a04728c742
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-10-18 11:05:12 +0300

    update

--- godot game/Player/player.gd
+++ godot game/Player/player.gd
@@ -1,5 +1,13 @@
 extends CharacterBody2D
 
+enum{
+	MOVE,
+	ATTACK,
+	ATTACK2,
+	ATTACK3,
+	BLOCK,
+	SLIDE
+}
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -8,41 +16,53 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = $AnimatedSprite2D
+@onready var animPlayer = $AnimationPlayer
 var health = 100
 var gold = 0
+var state = MOVE
 
 func _physics_process(delta):
+	
+	match state:
+		MOVE:
+			pass
+		ATTACK:
+			pass
+		ATTACK2:
+			pass
+		ATTACK3:
+			pass
+		BLOCK:
+			pass
+		SLIDE:
+			pass
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("fight") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			anim.play("Run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
-			anim.play("Idle")
 		
 	if direction == -1:
-		$AnimatedSprite2D.flip_h = true
+		anim.flip_h = true
 	elif direction == 1:
-		$AnimatedSprite2D.flip_h = false
+		anim.flip_h = false
 		
 	if health <= 0:
 		queue_free()
 		get_tree().change_scene_to_file("res://menu.tscn")
 		
 	if velocity.y > 0:
-		anim.play("Fall")
+		animPlayer.play("Fall")
 
 	move_and_slide()
+	
+func move_state ():
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			animPlayer.play("Run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			animPlayer.play("Idle")
